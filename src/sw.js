49dc41dedff461bea4064c9434c269349702bb34
royPark <royssama@gmail.com>
2025-03-31 21:50:31 +0900

    상단 이미지 추가

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'multiplication-game-v3';
+const CACHE_NAME = 'multiplication-game-v1';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -6,29 +6,21 @@ const urlsToCache = [
     '/icons/icon-32x32.png',
     '/icons/icon-96x96.png',
     '/icons/icon-144x144.png',
+    '/icons/icon-180x180.png',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
 ];
 
-// 서비스 워커 설치
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// 캐시 사용
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
+            .then(response => response || fetch(event.request))
     );
 }); 
